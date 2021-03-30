5530832d24e026ccaaab80fdf18699c066d1a2ed
Frantisek Hartman <frant.hartm@gmail.com>
2021-03-30 18:18:22 +0200

    Add dummy source2

--- jet-processor-classloader-v1/src/main/java/org/example/jet/DummySource.java
+++ jet-processor-classloader-v1/src/main/java/org/example/jet/DummySource.java
@@ -1,6 +1,7 @@
 package org.example.jet;
 
 import com.hazelcast.jet.pipeline.BatchSource;
+import com.hazelcast.jet.pipeline.SourceBuilder;
 import com.hazelcast.jet.pipeline.test.TestSources;
 
 public class DummySource {
@@ -8,4 +9,17 @@ public class DummySource {
     public static BatchSource<String> source() {
         return TestSources.items("1", "2", "3");
     }
+
+    public static BatchSource<String> source2() {
+        return SourceBuilder.batch("source", context -> new int[1])
+                .fillBufferFn((int[] obj, SourceBuilder.SourceBuffer<String> buffer) -> {
+                    if (obj[0] < 10) {
+                        buffer.add(Integer.toString(obj[0]++));
+                    } else {
+                        buffer.close();
+                    }
+                }).build();
+    }
+
+
 }
--- jet-processor-classloader-v2/src/main/java/org/example/jet/DummySource.java
+++ jet-processor-classloader-v2/src/main/java/org/example/jet/DummySource.java
@@ -1,11 +1,23 @@
 package org.example.jet;
 
 import com.hazelcast.jet.pipeline.BatchSource;
+import com.hazelcast.jet.pipeline.SourceBuilder;
 import com.hazelcast.jet.pipeline.test.TestSources;
 
 public class DummySource {
 
     public static BatchSource<String> source() {
         return TestSources.items("A", "B", "C");
+    }
+
+    public static BatchSource<String> source2() {
+        return SourceBuilder.batch("source", context -> new int[1])
+                .fillBufferFn((int[] obj, SourceBuilder.SourceBuffer<String> buffer) -> {
+                    if (obj[0] < 10) {
+                        buffer.add(Integer.toString(10 - obj[0]++));
+                    } else {
+                        buffer.close();
+                    }
+                }).build();
     }
 }
